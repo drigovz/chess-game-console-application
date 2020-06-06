7e533c604a683be726b8b627697bb3579428f7b1
Rodrigo Vaz <rodrigodp2014@gmail.com>
2020-06-06 14:37:09 -0300

    Começando a colcar peças no tabuleiro

--- XadrezConsole/Tabuleiro/Tabuleiro.cs
+++ XadrezConsole/Tabuleiro/Tabuleiro.cs
@@ -12,10 +12,16 @@
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
         }
-         
+
         public Peca peca(int linha, int coluna)
         {
             return pecas[linha, coluna];
+        }
+
+        public void ColocarPeca(Peca p, Posicao pos)
+        {
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
         }
     }
 }
